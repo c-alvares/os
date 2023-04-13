6bd43f94c5699f3d7173a22a2131667afd42aedb
c-alvares <cyrobrandaoalvares@gmail.com>
2023-04-13 11:39:32 -0300

    Criados controllers

---
+++ backend/src/controllers/colaborador.js
@@ -0,0 +1,56 @@
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+const iniciar = async (req, res) => {
+    res.render('index')
+}
+
+const login = async (req, res) => {
+    let colaborador = await prisma.colaborador.findMany({
+        where: {
+            AND: [{ matricula: Number(req.body.matricula) }, { pin: Number(req.body.pin) }]
+        },
+        select: {
+            matricula: true,
+            nome: true,
+            cargo: true,
+            setor: true,
+            pin: true,
+            abertas: {
+                select: {
+                    id: true,
+                    descricao: true,
+                    colaborador: true,
+                    executor: true,
+                    abertura: true,
+                    encerramento: true,
+                    comentarios: true
+                }
+            },
+            executadas: {
+                where: {
+                    encerramento: null
+                },
+                select: {
+                    id: true,
+                    descricao: true,
+                    colaborador: true,
+                    executor: true,
+                    abertura: true,
+                    encerramento: true,
+                    comentarios: true
+                }
+            }
+        }
+    })
+    if (colaborador.length > 0)
+        res.render('oss', { colaborador: colaborador[0] });
+    else {
+        res.redirect('/?erro=Matrícula ou PIN incorretos!')
+    }
+}
+
+module.exports = {
+    iniciar,
+    login
+}
---
+++ backend/src/controllers/comentario.js
@@ -0,0 +1,10 @@
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+const criar = async (req, res) => {
+    res.redirect('/')
+}
+
+module.exports = {
+    criar
+}
