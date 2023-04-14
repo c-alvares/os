fd793675c949bd352124ca4e0e652117be009cc3
c-alvares <cyrobrandaoalvares@gmail.com>
2023-04-14 08:29:45 -0300

    Tratamento de erros

--- backend/src/controllers/colaborador.js
+++ backend/src/controllers/colaborador.js
@@ -1,56 +1,71 @@
-const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 const iniciar = async (req, res) => {
-    res.render('index')
-}
+	res.render("index");
+};
 
 const login = async (req, res) => {
-    let colaborador = await prisma.colaborador.findMany({
-        where: {
-            AND: [{ matricula: Number(req.body.matricula) }, { pin: Number(req.body.pin) }]
-        },
-        select: {
-            matricula: true,
-            nome: true,
-            cargo: true,
-            setor: true,
-            pin: true,
-            abertas: {
-                select: {
-                    id: true,
-                    descricao: true,
-                    colaborador: true,
-                    executor: true,
-                    abertura: true,
-                    encerramento: true,
-                    comentarios: true
-                }
+    if (!isNaN(req.body.pin) && !isNaN(req.body.matricula)) {
+        let colaborador = await prisma.colaborador.findMany({
+            where: {
+                AND: [{ matricula: Number(req.body.matricula) }, { pin: Number(req.body.pin) }]
             },
-            executadas: {
-                where: {
-                    encerramento: null
+            select: {
+                matricula: true,
+                nome: true,
+                cargo: true,
+                setor: true,
+                pin: true,
+                abertas: {
+                    select: {
+                        id: true,
+                        descricao: true,
+                        colaborador: true,
+                        executor: true,
+                        abertura: true,
+                        encerramento: true,
+                        comentarios: true
+                    }
                 },
-                select: {
-                    id: true,
-                    descricao: true,
-                    colaborador: true,
-                    executor: true,
-                    abertura: true,
-                    encerramento: true,
-                    comentarios: true
+                executadas: {
+                    where: {
+                        encerramento: null
+                    },
+                    select: {
+                        id: true,
+                        descricao: true,
+                        colaborador: true,
+                        executor: true,
+                        abertura: true,
+                        encerramento: true,
+                        comentarios: true
+                    }
                 }
             }
+        })
+
+        let executores = await prisma.colaborador.findMany({
+            where: {
+                setor: "Manutenção"
+            },
+            select: {
+                matricula: true,
+                nome: true
+            }
+        })
+
+        if (colaborador.length > 0)
+            res.render('oss', { colaborador: colaborador[0], executores: executores });
+        else {
+            res.redirect('/?erro=Matrícula ou PIN incorretos!')
         }
-    })
-    if (colaborador.length > 0)
-        res.render('oss', { colaborador: colaborador[0] });
-    else {
+    } else {
         res.redirect('/?erro=Matrícula ou PIN incorretos!')
     }
 }
 
 module.exports = {
-    iniciar,
-    login
-}
+	iniciar,
+	login,
+};
