f4187e991c4ea248fbe58d32fd192af4bd79abdb
c-alvares <cyrobrandaoalvares@gmail.com>
2023-04-14 08:41:56 -0300

    Finalizado controller dos comentários

--- backend/src/controllers/comentario.js
+++ backend/src/controllers/comentario.js
@@ -2,7 +2,15 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 const criar = async (req, res) => {
-    res.redirect('/')
+    let parse = {
+        comentario: req.body.comentario,
+        os: Number(req.body.id),
+        colaborador: Number(req.body.matricula),
+    }
+    let os = await prisma.comentario.create({
+        data: parse
+    })
+    res.redirect('/?msg=Comentário criado com sucesso!')
 }
 
 module.exports = {
